45c3533a7d36b89878d9e6181d77cc343972fdd8
Admin <1q.expert@gmail.com>
2024-03-31 01:26:38 +0200

    last fix mobile view

--- src/js/script.js
+++ src/js/script.js
@@ -4,13 +4,15 @@ class MediaMobile {
       this.innerMenuNavbar = document.querySelector('.mobile_inner');
       this.contentSheet = document.querySelector('.content-sheet');
       this.mainContent = document.querySelector('main.content');
-      this.bindEvents();
-      this.handleResize(); 
+      if (this.menuTriggerBtn && this.innerMenuNavbar) {
+        this.bindEvents();
+      }
+      this.handleResize();
     }
   
     bindEvents() {
       this.menuTriggerBtn.addEventListener('click', () => this.toggleNavbarActive());
-      window.addEventListener('resize', () => this.handleResize());
+      window.addEventListener('resize', this.debounce(() => this.handleResize(), 250));
     }
   
     toggleNavbarActive() {
@@ -19,22 +21,27 @@ class MediaMobile {
     }
 
     handleResize() {
-      if (window.innerWidth <= 450) {
-        this.contentSheet.classList.add('content');
-        this.mainContent.classList.remove('content');
-      } else {
-        if (this.contentSheet.classList.contains('content')) {
-          this.contentSheet.classList.remove('content');
-        }
-        if (!this.mainContent.classList.contains('content')) {
-          this.mainContent.classList.add('content');
-        }
-      }
+      const isMobile = window.innerWidth <= 450;
+      this.contentSheet.classList.toggle('content', isMobile);
+      this.mainContent.classList.toggle('content', !isMobile);
     }
-  }
+
+    debounce(func, wait, immediate) {
+      let timeout;
+      return function() {
+        const context = this, args = arguments;
+        const later = function() {
+          timeout = null;
+          if (!immediate) func.apply(context, args);
+        };
+        const callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+      };
+    }
+}
   
 document.addEventListener('DOMContentLoaded', function () {
     new MediaMobile();
 });
-
-  
